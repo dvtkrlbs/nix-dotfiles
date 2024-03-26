0c03ec11091db09c7d441755f5cd0ca24bd5ac6f
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2024-03-26 22:35:34 +0000

    update lycalopex

--- hosts/lycalopex.nix
+++ hosts/lycalopex.nix
@@ -4,8 +4,35 @@
    nixpkgs,
    config,
    inputs,
+   lib,
    ...
 }: {
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    # registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    # nixPath = ["/etc/nix/path"];
+
+    package = pkgs.nixVersions.unstable;
+
+    settings = {
+      trusted-users = ["root" "dvtkrlbs"]; # For groups prepend @: "@admin"
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+    extraOptions = lib.optionalString (pkgs.system == "aarch64-darwin") ''
+      extra-platforms = x86_64-darwin aarch64-darwin;
+    '';
+    gc = {
+      automatic = true;
+    };
+  };
+
   hardware = {
     raspberry-pi."4" = {
       apply-overlays-dtmerge.enable = true;
