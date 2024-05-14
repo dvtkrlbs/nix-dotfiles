e4d02bfb1984bdffbc95506e2f802ce6c35f40de
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2024-05-14 15:33:18 +0300

    update elixir flake

--- templates/elixir/flake.nix
+++ templates/elixir/flake.nix
@@ -25,8 +25,38 @@
 #          inherit (self.checks.${system}.pre-commit-check) shellHook;
           buildInputs = with pkgs; [
             elixir_1_16
-            erlang_26
-          ];
+          ] ++ lib.optionals stdenv.isLinux [
+            # For ExUnit Notifier on Linux.
+            libnotify
+
+            # For file_system on Linux.
+            inotify-tools
+          ]
+          ++ lib.optionals stdenv.isDarwin ([
+            # For ExUnit Notifier on macOS.
+            terminal-notifier
+
+            # For file_system on macOS.
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+          ]);
+
+          shellHook = ''
+            # allows mix to work on the local directory
+            mkdir -p .nix-mix
+            mkdir -p .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
+
+            # concats PATH
+            export PATH=$MIX_HOME/bin:$PATH
+            export PATH=$MIX_HOME/escripts:$PATH
+            export PATH=$HEX_HOME/bin:$PATH
+
+            # enables history for IEx
+            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
+          '';
         };
       };
 
